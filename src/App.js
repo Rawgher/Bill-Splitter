80d473927c77d03341d944015a12faa314177d84
Roger Pender <rdpender3@gmail.com>
2023-07-06 12:24:02 -0700

    conditionally showing add friend form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
+import { useState } from 'react';
 import AddFriendForm from './components/AddFriendForm';
 import Button from './components/Button';
 import FriendsList from './components/FriendsList';
 import SplitForm from './components/SplitForm';
+import './App.css';
 
 function App() {
+  const [showAddButton, setShowAddButton] = useState(false);
+
+  function handleShowAddForm() {
+    setShowAddButton((show) => !showAddButton);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <AddFriendForm />
-        <Button>Add Friend</Button>
+        {showAddButton && <AddFriendForm />}
+        <Button onClick={handleShowAddForm}>{showAddButton ? 'Close Form' : 'Add Friend'}</Button>
       </div>
       <SplitForm />
     </div>
