72d6f89a0ffd143b77d5c96f38f499e0e4f2ce50
Roger Pender <rdpender3@gmail.com>
2023-07-07 12:12:48 -0700

    beginning split form states

--- src/components/SplitForm.js
+++ src/components/SplitForm.js
@@ -1,21 +1,32 @@
+import { useState } from 'react';
 import Button from './Button';
 
 export default function SplitForm({ selectedFriend }) {
+  const [bill, setBill] = useState('');
+  const [paidByYou, setPaidByYou] = useState('');
+  const [whoIsPaying, setWhoIsPaying] = useState('You');
+
+  let friendExpense = bill ? bill - paidByYou : '';
+
+  function handleYourPortion(e) {
+    setPaidByYou(Number(e.target.value) > bill ? paidByYou : Number(e.target.value));
+  }
+
   return (
     <form className="form-split-bill">
       <h2>Split the bill with {selectedFriend.name}</h2>
 
       <label>💵 Bill Total</label>
-      <input type="text" />
+      <input type="text" value={bill} onChange={(e) => setBill(Number(e.target.value))} />
 
       <label>💰 Your portion</label>
-      <input type="text" />
+      <input type="text" value={paidByYou} onChange={handleYourPortion} />
 
       <label>😁 {selectedFriend.name}'s portion</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={friendExpense} />
 
       <label>💳 Who is paying?</label>
-      <select>
+      <select value={whoIsPaying} onChange={(e) => setWhoIsPaying(e.target.value)}>
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
